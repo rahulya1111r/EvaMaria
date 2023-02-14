b5f4c0e11e99bdec696d5bfb65df8f5b2f745f2d
rahulya1111r <116951164+rahulya1111r@users.noreply.github.com>
2023-02-14 19:11:19 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -18,3 +18,20 @@ async def media(bot, message):
     media.file_type = file_type
     media.caption = message.caption
     await save_file(media)
+    
+@Client.on_message(filters.command("savefile") & filters.user(ADMINS))
+async def start(client, message):
+    try:
+        for file_type in ("document", "video", "audio"):
+        media = getattr(message.reply_to_message , file_type, None)
+        if media is not None:
+            break
+        else:
+            return
+
+        media.file_type = file_type
+        media.caption = message.reply_to_message.caption
+        await save_file(media)
+        await message.reply_text("**Saved In DB**")
+    except Exception as e:
+        await message.reply_text(f"**Error :- {str(e)}**")
